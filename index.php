5f1ada9dadfda04e76e0c3d75b53370f8cb3ae3b
Matt Salvadore <mattsalvadore@MacBook-Pro.local>
2018-09-05 19:15:01 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Jenkins Tutorial</title>
+  </head>
+  <body>
+    <h1><?php print date('Y'); ?></h1>
+  </body>
+</html>
